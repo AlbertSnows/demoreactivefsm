441a8b9527d91bff8cff38ea534ac961f5d005ab
Albert Snow <ajsnow2012@gmail.com>
2023-09-19 12:59:57 -0500

    adding book controller to test lombok fixing tests

--- src/test/java/com/example/demo/EntryTests.java
+++ src/test/java/com/example/demo/EntryTests.java
@@ -1,13 +1,19 @@
 package com.example.demo;
 
+import com.example.demoreactivefsm.Entry;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
+@ContextConfiguration(classes = Entry.class)
 class EntryTests {
 
 	@Test
 	void contextLoads() {
+		assertThat(1 + 1).isEqualTo(2);
 	}
 
 }
--- src/test/java/com/example/demo/GreetingRouterTest.java
+++ src/test/java/com/example/demo/GreetingRouterTest.java
@@ -1,11 +1,13 @@
 package com.example.demo;
 
+import com.example.demoreactivefsm.Entry;
 import com.example.demoreactivefsm.Greeting;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
@@ -14,6 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ExtendWith(SpringExtension.class)
 //  We create a `@SpringBootTest`, starting an actual server on a `RANDOM_PORT`
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(classes = Entry.class)
 class GreetingRouterTest {
 	// Spring Boot will create a `WebTestClient` for you,
 	// already configure and ready to issue requests against "localhost:RANDOM_PORT"
