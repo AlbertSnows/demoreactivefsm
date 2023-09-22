16bd3c87e80bc87b58514238bf4584f610738f4f
Albert Snow <ajsnow2012@gmail.com>
2023-09-21 22:39:39 -0500

    trying to migrate

--- V1__Base_version.sql
+++ V1__Base_version.sql
@@ -1,13 +1,15 @@
--- db/migration/V1__Initial.sql
+-- db/migration/V1__Base_version.sql
 -- Description: Initial database schema
 -- Create a sample table
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     user_id serial PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
-    created_at TIMESTAMP NOT NULL);
+    created_at TIMESTAMP NOT NULL
+    );
 
---DROP TABLE IF EXISTS students;
+--DROP TABLE IF EXISTS users;
 --CREATE TABLE students(
 --    id SERIAL NOT NULL PRIMARY KEY ,
 --    firstname VARCHAR(250),
