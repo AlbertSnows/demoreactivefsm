954d28fe19373fbd058bd9507870b0d4e7772739
Albert Snow <ajsnow2012@gmail.com>
2023-09-22 00:58:44 -0500

    switching to yaml

--- src/main/resources/db/migration/V1__Base_version.sql
+++ src/main/resources/db/migration/V1__Base_version.sql
@@ -1,17 +1,18 @@
 -- db/migration/V1__Base_version.sql
 -- Description: Initial database schema
 -- Create a sample table
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-    user_id serial PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    created_at TIMESTAMP NOT NULL
-    );
-
 --DROP TABLE IF EXISTS users;
---CREATE TABLE students(
---    id SERIAL NOT NULL PRIMARY KEY ,
---    firstname VARCHAR(250),
---    lastname VARCHAR(250),
---    age integer);
+--CREATE TABLE users (
+--    user_id serial PRIMARY KEY,
+--    username VARCHAR(50) NOT NULL,
+--    email VARCHAR(100) NOT NULL UNIQUE,
+--    created_at TIMESTAMP NOT NULL
+--    );
+
+DROP TABLE IF EXISTS students;
+CREATE TABLE students(
+    id SERIAL NOT NULL PRIMARY KEY ,
+    firstname VARCHAR(250),
+    lastname VARCHAR(250),
+    age integer
+    );
