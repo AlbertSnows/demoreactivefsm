8e7eace0619b71e6e455ae11e941fa7797d93777
Albert Snow <ajsnow2012@gmail.com>
2023-09-23 16:21:53 -0500

    adding duration example

--- src/main/java/com/example/demoreactivefsm/routing/student/StudentController.java
+++ src/main/java/com/example/demoreactivefsm/routing/student/StudentController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+
 @RestController
 @RequestMapping("/api/v1/students")
 @RequiredArgsConstructor
@@ -19,7 +21,7 @@ public class StudentController {
 
 	@GetMapping
 	Flux<Student> findAll() {
-		return service.findAll();
+		return service.findAll().delayElements(Duration.ofSeconds(1));
 	}
 
 	@GetMapping("/{id}")
