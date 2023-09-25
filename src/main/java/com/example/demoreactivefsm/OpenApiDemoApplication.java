5a45bb42c206a9583b5e97a7a24c2ac75c586697
Albert Snow <ajsnow2012@gmail.com>
2023-09-24 22:13:48 -0500

    trying to get openapi working

---
+++ src/main/java/com/example/demoreactivefsm/OpenApiDemoApplication.java
@@ -0,0 +1,11 @@
+package com.example.demoreactivefsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiDemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(OpenApiDemoApplication.class, args);
+	}
+}
